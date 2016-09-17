854c8956f6bf5ff52b66b5ba37283004ee4d4bbf
Andreas Stenger <regnets@users.noreply.github.com>
2016-09-17 17:27:27 +0200

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,5 +5,6 @@ git fetch --all
 git reset --hard origin/master
 git pull origin master
 chown -R openhab:openhab *
-chmod -R 644 *
+chmod -R 777 *
 chmod +x scripts/update.sh
+systemctl restart openhab
